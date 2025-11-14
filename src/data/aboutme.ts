3a0d709575148ee4796884d7ee8ca090f9b255d2
Elif Akata <eliaka@users.noreply.github.com>
2025-11-14 12:43:24 +0100

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -23,13 +23,10 @@ export const aboutMe: AboutMe = {
   institution: "Helmholtz Munich & University of Tübingen",
   // Note that links work in the description
   description:
-    `I’m a final year PhD student in the Computer Science Department at the University of Tübingen (where I am also an <a href='https://imprs.is.mpg.de/'>IMPRS-IS</a> scholar) and the <a href='https://hcai-munich.com/'>Helmholtz Institute for Human-Centered AI</a>. I am fortunate to be advised by <a href='https://hcai-munich.com/eric.html/'>Eric Schulz</a> and <a href='https://bethgelab.org/'>Matthias Bethge</a>.
-    
-    My research lies in the intersection of cognitive science and machine learning. In particular, I’m interested in understanding how LLMs behave as social, collaborative agents and how we can design systems that effectively interact, adapt, and communicate in dynamic environments.
-    
-    Prior to my PhD, I graduated from University of Tübingen and Saarland University in Germany, with a MSc and BSc in Computer Science.
-    
-    In my free time, I like drawing (recently got into designing logos and journal covers), playing tennis, and watching The Traitors (my favourite one is the UK version).`,
+    `<p>I’m a final year PhD student in the Computer Science Department at the University of Tübingen (where I am also an <a href='https://imprs.is.mpg.de/'>IMPRS-IS</a> scholar) and the <a href='https://hcai-munich.com/'>Helmholtz Institute for Human-Centered AI</a>. I am fortunate to be advised by <a href='https://hcai-munich.com/eric.html/'>Eric Schulz</a> and <a href='https://bethgelab.org/'>Matthias Bethge</a>.</p> 
+    <p>My research lies in the intersection of cognitive science and machine learning. In particular, I’m interested in understanding how LLMs behave as social, collaborative agents and how we can design systems that effectively interact, adapt, and communicate in dynamic environments.</p> 
+    <p>Prior to my PhD, I graduated from University of Tübingen and Saarland University in Germany, with a MSc and BSc in Computer Science.</p>
+    <p>In my free time, I like drawing (recently got into designing logos and journal covers), playing tennis, and watching The Traitors (my favourite one is the UK version).</p>`,
   email: "elif.akata@helmholtz-munich.de",
   imageUrl:
     "/aboutme-img.jpg",
