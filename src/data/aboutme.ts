f619d68863f84f2c01ce2ef7d7f321b9e2a3a9ff
Elif Akata <eliaka@users.noreply.github.com>
2025-12-06 10:18:42 +0100

    update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -26,7 +26,7 @@ export const aboutMe: AboutMe = {
     `<p>I’m a final year PhD student in the Computer Science Department at the University of Tübingen (where I am also an <a href='https://imprs.is.mpg.de/'>IMPRS-IS</a> scholar) and the <a href='https://hcai-munich.com/'>Helmholtz Institute for Human-Centered AI</a> advised by <a href='https://hcai-munich.com/eric.html'>Eric Schulz</a> and <a href='https://bethgelab.org/'>Matthias Bethge</a>.</p> 
     <p>My research lies in the intersection of cognitive science and machine learning. In particular, I’m interested in understanding how LLMs behave as social, collaborative agents and how we can design systems that effectively interact, adapt, and communicate in dynamic environments.</p>
     <p>Prior to my PhD, I graduated from the University of Tübingen and Saarland University in Germany, with a MSc and BSc in Computer Science.</p>
-    <p>In my free time, I like drawing (recently got into designing logos and journal covers), playing tennis, and watching The Traitors (my favourite one is the UK version).</p>`,
+    <p>In my free time, I like drawing (recently got into designing logos and journal covers), and playing tennis.</p>`,
   email: "elif.akata@helmholtz-munich.de",
   imageUrl:
     "/aboutme-img.jpg",
