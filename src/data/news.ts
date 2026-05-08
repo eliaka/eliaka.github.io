8b222eb558e1380fd1084d5de3450e9dff238175
Elif Akata <eliaka@users.noreply.github.com>
2026-05-08 11:01:54 +0100

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,6 +7,12 @@ export interface News {
 
 export const newsData: News[] = [
   {
+    date: "May 2026",
+    title: "",
+    description: "I was in Tangier, Morocco, to present our paper at AISTATS.",
+    link: "https://virtual.aistats.org/virtual/2026/poster/13708",
+  },
+  {
     date: "February 2026",
     title: "",
     description: "I gave a talk at the Human Information Processing Lab at Oxford.",
